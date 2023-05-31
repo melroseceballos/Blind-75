42e1dc9fb7a0b866549e3b84823674bb79ef65dd
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-30 17:19:04 -0700

    Update 49. GroupAnagrams.js

--- Arrays/49. GroupAnagrams.js
+++ Arrays/49. GroupAnagrams.js
@@ -1,4 +1,43 @@
 /** PROBLEM:
 Given an array of strings strs, group the anagrams together.
 You can return the answer in any order.
+
+    PSEUDOCODE
+- create a variable to store your key
+- loop through the array of strings
+- split them so they are individual characters
+- sort through them to sort them alphabetically 
+- join them so those characters are together
+- create a conditional that checks if they key is already in there
+    - if true, simple push the key in the map
+    - if false, create a key in the map
+- return the value using Object.values to create an array
 */
+
+// SOLUTION:
+groupAnagrams = (strs) => {
+    // stores the keys
+  let count = {}
+  // dont forget to add const/let in loop
+for (const letter of strs){
+    // this line sorts through the strings
+/* example: tea,eat,ate all have after being split e,t,a
+the sort alphabeticizes it = a,e,t
+and then joins them to be like this = "aet"
+so now any word that contains the letter aet 
+will be added as a value 
+*/
+const key = letter.split("").sort().join("")
+
+if(count[key]){
+count[key].push(letter)
+}else{
+  count[key] = [letter]
+}
+}
+  return Object.values(count)
+}
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+
