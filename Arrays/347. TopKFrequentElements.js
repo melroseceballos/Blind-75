c33e4cc233c931a8b3058192efdaa327348f49d6
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-31 20:28:15 -0700

    Update 347. TopKFrequentElements.js

--- Arrays/347. TopKFrequentElements.js
+++ Arrays/347. TopKFrequentElements.js
@@ -9,3 +9,25 @@ You may return the answer in any order.
 * create an array to store frequent elements 
 
 */
+
+
+// SOLUTION:
+const frequentElements = (arr, k) => {
+  if (arr.length <= 1) return arr;
+
+  const count = {};
+  const frequent = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    count[element] = (count[element] || 0) + 1;
+
+    if (count[element] === k) {
+      frequent.push(element);
+    }
+  }
+
+  return frequent;
+};
+
+console.log(frequentElements([1, 1, 1, 2, 2, 3], 2));
+console.log(frequentElements([1], 1));
