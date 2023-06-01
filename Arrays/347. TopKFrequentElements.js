551f10ad4e067f4def1b712aeb9c42e76a2bb614
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-31 18:38:03 -0700

    Update 347. TopKFrequentElements.js

--- Arrays/347. TopKFrequentElements.js
+++ Arrays/347. TopKFrequentElements.js
@@ -2,3 +2,8 @@
 Given an integer array nums and an integer k, return the k most frequent elements. 
 You may return the answer in any order.
 */
+
+/* PSEUDOCODE
+- create a condition that checks if arr is <= to 1
+  if so, return array
+*/
